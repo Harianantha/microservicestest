a3d10d5b8fe05f516ee5840a8adf375c84d405ff
Harianantha <hariharan.anantharaman@gmail.com>
2016-02-06 17:10:52 +0530

    changes to test build and deployment

--- microservicestest/src/main/java/hello/CategoriesController.java
+++ microservicestest/src/main/java/hello/CategoriesController.java
@@ -16,12 +16,15 @@ public class CategoriesController {
     public List<Category> categories() {
         Category books=new Category(1,"Books");
         Category clothing=new Category(2,"Clothing");
+        Category electronics=new Category(30,"Electronics");
         
         List<Category> booksChildCategory= new ArrayList<Category>();
         List<Category> clothingChildCategory= new ArrayList<Category>();
+        List<Category> electronicsCategory= new ArrayList<Category>();
         
         List<Product> bookProducts= new ArrayList<Product>();
         List<Product> clothingProducts= new ArrayList<Product>();
+        List<Product> electronicsProducts= new ArrayList<Product>();
         
         
         Product book1=new Product(1,"MenAreFrom","Men are From Mars,Women are from Venus","Book about Psychoology",new Float("12.0"));
@@ -32,6 +35,17 @@ public class CategoriesController {
         
         Product cloth1=new Product(1,"Levis1","Levis 501","Classic Bluue Denim",new Float("30.0"));
         Product cloth2=new Product(1,"Formal1","Classic White Shirt","Formals",new Float("10.0"));
+        
+        clothingProducts.add(cloth1);
+        clothingProducts.add(cloth2);
+        clothing.getProducts().addAll(clothingProducts);
+        
+        
+        Product electronics1=new Product(1,"iPAD","iPAD Air","Great tablet",new Float("1000.0"));
+        Product electronics2=new Product(1,"SufacePro","Suface pro 128GB","Useful tablet",new Float("1000.0"));
+        electronicsProducts.add(electronics1);
+        electronicsProducts.add(electronics2);
+        electronics.getProducts().addAll(electronicsProducts);
         
         clothingProducts.add(cloth1);
         clothingProducts.add(cloth2);
@@ -49,9 +63,16 @@ public class CategoriesController {
         clothingChildCategory.add(shirts);
         clothing.getChildCategories().addAll(clothingChildCategory);
         
+        Category cellphones=new Category(7,"Cellphones");
+        Category tablets=new Category(8,"Tablets");
+        electronicsCategory.add(cellphones);
+        electronicsCategory.add(tablets);
+        electronics.getChildCategories().addAll(electronicsCategory);
+        
         List<Category> returnList=new ArrayList<Category>();
         returnList.add(books);
         returnList.add(clothing);
+        returnList.add(electronics);
         return returnList;
         
         
